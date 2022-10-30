66a9afaf38fedf617907993fe258a45b4ab210d5
MW <mw@Maxyms-MacBook-Pro-8.local>
2022-10-30 16:38:40 -0600

    test for add

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,18 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+
+    @Before
+    public void createList() {
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 
 }
